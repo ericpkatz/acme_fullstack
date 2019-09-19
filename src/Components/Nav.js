32f3d26d3fdd3f075d55e393babcab14539047be
Eric P. Katz <ericpkatz@gmail.com>
2019-09-19 18:54:17 -0400

    added route and link

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -2,21 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-const Nav = ({ count, location: { pathname } })=> {
+const Nav = ({ count, activeCount, location: { pathname } })=> {
   return (
     <div>
       <h1>The Acme Users</h1>
       <nav>
         <Link to='/' className={ pathname === '/' ? 'active': ''}>Home</Link>
         <Link to='/users' className={ pathname === '/users' ? 'active': ''}>Users({ count })</Link>
+        <Link to='/users/active' className={ pathname === '/users/active' ? 'active': ''}>Active Users({ activeCount })</Link>
       </nav>
     </div>
   );
 };
 
 const mapStateToProps = ({ users })=> {
+  const activeCount = users.filter( user => user.active).length;
   return {
-    count: users.length
+    count: users.length,
+    activeCount
   };
 };
 
